e62df4a8d996fc935027b288dd431de0f8409d6d
kamal johnson <kamaljohnson12345@gmail.com>
2019-05-11 10:42:50 +0530

    Init Lazer editor script

---
+++ MazeGame/Assets/Scripts/Editor/Items/Interactable/Laser/LazerEditorScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Items.Activators.Button;
+using UnityEditor;
+using UnityEngine;
+
+namespace LevelEditor.Items.Intractable.Lazer
+{
+    public class LazerEditorScript : Editor, ITem, ITemButtonInteraction
+    {
+        public void Init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckValuesSet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddButtonLink(Button button)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditButtonLink()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveButtonLink(int buttonId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
