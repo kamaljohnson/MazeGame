0bf4e66e77a2457dc6e371304bda6cdd0622ba48
kamal johnson <kamaljohnson12345@gmail.com>
2019-07-13 11:10:45 +0530

    adding Blade | Hammer | Guardian basic script

---
+++ MazeGame/Assets/Scripts/Editor/Items/Enemies/Blade/BladeEditorScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditor.Items.Enemies.Blade
+{
+    public class BladeEditorScript : MonoBehaviour
+    {
+        
+    }
+}
---
+++ MazeGame/Assets/Scripts/Editor/Items/Enemies/Hammer/HammerEditorScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditor.Items.Enemies.Hammer
+{
+    public class HammerEditorScript : MonoBehaviour
+    {
+    
+        
+    }
+}
