211da135f941a7d5f5f95fac1fa79dfd96cbd950
kamal johnson <kamaljohnson12345@gmail.com>
2019-07-13 11:14:46 +0530

    init Enemie Scripts

---
+++ MazeGame/Assets/Scripts/Game/Items/Enemies/Guardian/Guardian.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Items.Enemies.Guardian
+{
+    public class Guardian : MonoBehaviour
+    {
+        
+    }
+}
