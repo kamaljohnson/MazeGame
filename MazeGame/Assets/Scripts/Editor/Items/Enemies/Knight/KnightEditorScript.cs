a65984577d9d912a7a2101e5afaa2f66e1e4af25
kamal johnson <kamaljohnson12345@gmail.com>
2019-07-16 16:04:42 +0530

    updating visuals | knight init

---
+++ MazeGame/Assets/Scripts/Editor/Items/Enemies/Knight/KnightEditorScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditor.Items.Enemies.Knight{
+    public class KnightEditorScript : MonoBehaviour
+    {
+
+    }
+}
